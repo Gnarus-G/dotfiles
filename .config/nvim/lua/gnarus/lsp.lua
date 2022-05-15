9f1ac47c9f65a9861fb7f25387e3ebb1dd044e86
gnarus-g <g.avet.gnaritas@gmail.com>
2022-05-15 18:06:28 -0400

    select lsp suggestion with Tab as well

--- .config/nvim/lua/gnarus/lsp.lua
+++ .config/nvim/lua/gnarus/lsp.lua
@@ -54,7 +54,10 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({
             select = true
-        }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = cmp.mapping.confirm({
+            select = true
+        }) -- Accept currently selected item with Tab press as well.
     }),
     sources = cmp.config.sources({{
         name = 'nvim_lsp'
