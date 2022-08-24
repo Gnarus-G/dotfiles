045be28c2beaf27d8f51c22218e743587b06b111
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-08-24 15:52:54 -0400

    feat: configs for wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+  enable_tab_bar = false,
+  font = wezterm.font "Fira Code",
+  harfbuzz_features = { "zero", "ss01", "cv05" },
+  color_scheme = "tokyonight",
+  window_background_opacity = 0.88,
+  initial_cols = 140,
+  initial_rows = 50,
+}
