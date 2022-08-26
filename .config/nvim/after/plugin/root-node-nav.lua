17f6f98a4b8932664da86fea6ad0f27ef40207dd
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-08-26 19:56:14 -0400

    feat: improving the <up> <down> ts node navigation with v:count for multiple jumps easily

--- .config/nvim/after/plugin/root-node-nav.lua
+++ .config/nvim/after/plugin/root-node-nav.lua
@@ -1,5 +1,4 @@
 local ts_utils = require "nvim-treesitter.ts_utils"
-local map = require("gnarus.keymap").map
 
 local function getCurrentRootLevelNode()
   local curr_node = ts_utils.get_node_at_cursor()
@@ -20,23 +19,51 @@ local function getCurrentRootLevelNode()
   return curr_node
 end
 
-vim.api.nvim_create_user_command("TSPrevRootLevelNode", function()
-  local node = getCurrentRootLevelNode()
-  local prev = node:prev_sibling()
-  if prev ~= nil then
-    ts_utils.goto_node(prev)
-    print("Current Treesitter Node is of type:", prev:type())
+local function jump_from(node, jump_count, next_node)
+  local to_node = node;
+  for _ = 1, jump_count ~= 0 and jump_count or 1 do
+    if next_node(to_node) == nil then
+      break
+    end
+    to_node = next_node(to_node)
   end
-end, {})
+  return to_node;
+end
 
-vim.api.nvim_create_user_command("TSNextRootLevelNode", function()
-  local node = getCurrentRootLevelNode()
-  local next = node:next_sibling()
-  if next ~= nil then
-    ts_utils.goto_node(next)
-    print("Current Treesitter Node is of type:", next:type())
+local function go_to_node(node)
+  if node ~= nil then
+    ts_utils.goto_node(node)
+    print("Current Treesitter Node is of type:", node:type())
   end
-end, {})
+end
 
-map("n", "<up>", ":TSPrevRootLevelNode<cr>")
-map("n", "<down>", ":TSNextRootLevelNode<cr>")
+local function getNextRootLevelNode(jump_count)
+  local rl_node = getCurrentRootLevelNode()
+  local next = jump_from(rl_node, jump_count, function(node)
+    return node:next_sibling()
+  end)
+  go_to_node(next)
+end
+
+local function getPreviousRootLvlNode(jump_count)
+  local rl_node = getCurrentRootLevelNode()
+  local prev = jump_from(rl_node, jump_count, function(node)
+    return node:prev_sibling()
+  end)
+  go_to_node(prev)
+end
+
+vim.api.nvim_create_user_command("TSPrevRootLevelNode", function(arg)
+  getPreviousRootLvlNode(arg.count)
+end, {
+  count = true
+})
+
+vim.api.nvim_create_user_command("TSNextRootLevelNode", function(arg)
+  getNextRootLevelNode(arg.count)
+end, {
+  count = true
+})
+
+vim.keymap.set("n", "<up>", function() getPreviousRootLvlNode(vim.v.count) end)
+vim.keymap.set("n", "<down>", function() getNextRootLevelNode(vim.v.count) end)
