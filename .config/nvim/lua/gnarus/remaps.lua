0d2bdbc32ef9a9a560dce3127f27826cb7a3f06a
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-08-31 19:20:54 -0400

    feat: mapping to move a selection around the file, shout out @theprimageon

--- .config/nvim/lua/gnarus/remaps.lua
+++ .config/nvim/lua/gnarus/remaps.lua
@@ -6,3 +6,6 @@ map("i", "jk", "<Esc>")
 map("n", "<leader>e", "<cmd>Lex 25<cr>")
 
 map("n", "<leader>F", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+
+map("v", "J", ":m '>+1<cr>gv=gv")
+map("v", "K", ":m '<-2<cr>gv=gv")
