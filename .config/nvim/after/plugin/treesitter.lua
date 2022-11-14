3e3cca393a58fcdf8c25727230e15b16a240ac60
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-11-13 19:12:16 -0500

    experiment: trying some now treesitter configs

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,9 @@
 require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "lua" },
+  autotag = {
+    enable = true,
+    filetypes = { "html", "xml", "tsx" }
+  },
   highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true }
