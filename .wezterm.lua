69bfdb90489852b57f4a9eb8e6e45d9ce854aa90
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-01-25 12:37:38 -0500

    feat: more text on screen with wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 return {
   enable_tab_bar = false,
   font = wezterm.font "Fira Code",
+  font_size = 10,
   harfbuzz_features = { "zero", "ss01", "cv05" },
   color_scheme = "tokyonight",
   window_background_opacity = 0.88,
