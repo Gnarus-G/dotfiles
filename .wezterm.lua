b143a9e4681515ed48c91fa0e6a1e2ec392e138c
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-01-27 13:31:18 -0500

    feat: some workspace config for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,13 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
 
 return {
-  enable_tab_bar = false,
+  enable_tab_bar = true,
   font = wezterm.font "Fira Code",
   font_size = 10,
   harfbuzz_features = { "zero", "ss01", "cv05" },
@@ -9,4 +15,37 @@ return {
   window_background_opacity = 0.88,
   initial_cols = 140,
   initial_rows = 50,
+  keys = {
+    -- Switch to the default workspace
+    {
+      key = 'y',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+        name = 'default',
+      },
+    },
+    -- Switch to a monitoring workspace, which will have `top` launched into it
+    {
+      key = 'd',
+      mods = 'CTRL|SHIFT',
+      action = act.SwitchToWorkspace {
+        name = 'Dev',
+        spawn = {
+          args = { 'nvim' },
+          cwd = '/home/gnarus/d'
+        },
+      },
+    },
+    -- Create a new workspace with a random name and switch to it
+    { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+    -- Show the launcher in fuzzy selection mode and have it list all workspaces
+    -- and allow activating one.
+    {
+      key = 'd',
+      mods = 'ALT',
+      action = act.ShowLauncherArgs {
+        flags = 'FUZZY|WORKSPACES',
+      },
+    },
+  },
 }
