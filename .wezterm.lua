a1fb76abc07e28f1cd105fb5bb3a555119ddcdc0
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-01-27 19:55:58 -0500

    feat: removing workspaces configs, since I'm prefering tabs over workspaces for now

--- .wezterm.lua
+++ .wezterm.lua
@@ -36,37 +36,6 @@ return {
   initial_rows = 50,
   launch_menu = launch_menu,
   keys = {
-    -- Switch to the default workspace
-    {
-      key = 'y',
-      mods = 'CTRL|SHIFT',
-      action = act.SwitchToWorkspace {
-        name = 'default',
-      },
-    },
-    -- Switch to a monitoring workspace, which will have `top` launched into it
-    {
-      key = 'd',
-      mods = 'CTRL|SHIFT',
-      action = act.SwitchToWorkspace {
-        name = 'Dev',
-        spawn = {
-          args = { 'nvim' },
-          cwd = '/home/gnarus/d'
-        },
-      },
-    },
-    -- Create a new workspace with a random name and switch to it
-    { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
-    -- Show the launcher in fuzzy selection mode and have it list all workspaces
-    -- and allow activating one.
-    {
-      key = 'w',
-      mods = 'ALT',
-      action = act.ShowLauncherArgs {
-        flags = 'FUZZY|WORKSPACES',
-      },
-    },
     -- Show launcher menu items
     {
       key = 'd',
