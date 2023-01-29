cdfccfb129ba2247d923f6860a98e490a3e641c7
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-01-29 14:56:55 -0500

    fix: launch menu was recalculated too oftern unecessarily, so I'm getting rid of it for now

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,26 +19,6 @@ wezterm.on(
   end
 )
 
-local launch_menu = {}
-
-local projects_dir = wezterm.home_dir .. "/d/";
--- Find all my git projects under the directory ~/d/
-local success, stdout, stderr = wezterm.run_child_process { "fd", "-HIg", "**/.git", "--base-directory", projects_dir,
-  "--strip-cwd-prefix", "-x", "echo", "{//}" }
-
-if success then
-  for _, line in ipairs(wezterm.split_by_newlines(stdout)) do
-    local path = projects_dir .. line;
-    table.insert(launch_menu, {
-      label = line,
-      args = { 'nvim' },
-      cwd = path
-    })
-  end
-else
-  wezterm.log_error("Couldn't find projects with fd - " .. stderr);
-end
-
 return {
   enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
@@ -51,17 +31,7 @@ return {
   window_background_opacity = 0.88,
   initial_cols = 140,
   initial_rows = 50,
-  launch_menu = launch_menu,
   keys = {
-    -- Show launcher menu items
-    {
-      key = 'd',
-      mods = 'CTRL|SHIFT',
-      action = act.ShowLauncherArgs {
-        flags = 'FUZZY|LAUNCH_MENU_ITEMS',
-        title = "Projects and stuff"
-      },
-    },
     {
       key = '"',
       mods = 'CTRL|SHIFT',
