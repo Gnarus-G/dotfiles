9c5a216032bb938c91876cdcfdbb4f406a1a2b3a
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-04-07 09:13:01 -0400

    fix: only run null-ls eslintd in node projects

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -9,8 +9,16 @@ null_ls.setup({
     null_ls.builtins.formatting.prettierd.with({
       extra_filetypes = { "astro" },
     }),
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ "package.json" })
+      end,
+    }),
+    null_ls.builtins.code_actions.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ "package.json" })
+      end,
+    }),
     null_ls.builtins.code_actions.gitsigns,
     require("typescript.extensions.null-ls.code-actions"),
   },
