30bd4c1c3830129f496fb83b97a338da53334abc
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-06 13:22:17 -0500

    fix: removing a useless plugin

--- .config/nvim/lua/gnarus/plugins.lua
+++ .config/nvim/lua/gnarus/plugins.lua
@@ -30,7 +30,6 @@ return packer.startup(function(use)
   use 'tpope/vim-fugitive'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'ThePrimeagen/git-worktree.nvim'
   use 'ThePrimeagen/harpoon'
 
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
