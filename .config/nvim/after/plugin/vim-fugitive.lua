2874350381ec71945e3447879140af48216dc69b
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-21 21:06:43 -0400

    feat: simpler vim-fugitive config

--- .config/nvim/after/plugin/vim-fugitive.lua
+++ .config/nvim/after/plugin/vim-fugitive.lua
@@ -1,12 +1,10 @@
-
 vim.keymap.set("n", "<leader>gs", ":G<CR>")
 vim.keymap.set("n", "<leader>gg", ":G<CR>")
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local Gnarus_Fugitive = vim.api.nvim_create_augroup("Gnarus_Fugitive", {})
 
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-  group = ThePrimeagen_Fugitive,
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = Gnarus_Fugitive,
   pattern = "*",
   callback = function()
     if vim.bo.ft ~= "fugitive" then
@@ -15,14 +13,6 @@ autocmd("BufWinEnter", {
 
     local bufnr = vim.api.nvim_get_current_buf()
     local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "<leader>p", function()
-      vim.cmd.Git('push')
-    end, opts)
-
-    -- rebase always
-    vim.keymap.set("n", "<leader>P", function()
-      vim.cmd.Git({ 'pull', '--rebase' })
-    end, opts)
 
     -- NOTE: It allows me to easily set the branch i am pushing and any tracking
     -- needed if i did not set the branch up correctly
