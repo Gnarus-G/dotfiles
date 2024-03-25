a4dee071275a1f6e16ca6ee387dfdf660ed66293
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-24 22:40:38 -0400

    fix: TailwindSort is asynchronus so format on save won't work

--- .config/nvim/after/plugin/tailwindcss.lua
+++ .config/nvim/after/plugin/tailwindcss.lua
@@ -14,14 +14,6 @@ require("tailwind-tools").setup(
     },
   })
 
--- Format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("FormatOnSave", { clear = false }),
-  callback = function()
-    vim.cmd ":TailwindSort"
-  end
-})
-
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = vim.api.nvim_create_augroup("TailwindFold", { clear = true }),
   callback = function()
