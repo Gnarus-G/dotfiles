2a8ccc1991537803608a4e63995e7cbdb5ef0469
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-25 11:14:38 -0400

    fix: null ls playing badly with nvim tree and typescript-tools

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -5,6 +5,13 @@ if not ok then
 end
 
 null_ls.setup({
+  --- @param bufnr number
+  --- @return boolean
+  should_attach = function(bufnr)
+    local buf_name = vim.api.nvim_buf_get_name(bufnr);
+    local excluded = buf_name:match("^fugitive://") or buf_name:match("NvimTree");
+    return not excluded
+  end,
   sources = {
     null_ls.builtins.formatting.prettierd.with({
       extra_filetypes = { "astro" },
