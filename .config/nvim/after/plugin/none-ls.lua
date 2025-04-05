651893e38206faf6e44c42046acb7dbc66af189a
gnarus-g <g.avet.gnaritas@gmail.com>
2025-04-05 19:00:43 -0400

    Replace null-ls with none-ls

--- .config/nvim/after/plugin/none-ls.lua
+++ .config/nvim/after/plugin/none-ls.lua
@@ -1,10 +1,10 @@
-local ok, null_ls = pcall(require, "null-ls");
+local ok, none_ls = pcall(require, "none-ls");
 
 if not ok then
   return
 end
 
-null_ls.setup({
+none_ls.setup({
   --- @param bufnr number
   --- @return boolean
   should_attach = function(bufnr)
@@ -13,21 +13,21 @@ null_ls.setup({
     return not excluded
   end,
   sources = {
-    null_ls.builtins.formatting.prettierd.with({
+    none_ls.builtins.formatting.prettierd.with({
       extra_filetypes = { "astro" },
     }),
-    null_ls.builtins.diagnostics.eslint_d.with({
+    none_ls.builtins.diagnostics.eslint_d.with({
       condition = function(utils)
         return utils.root_has_file({ "package.json" })
             and utils.root_has_file_matches(".eslintrc.*")
       end,
     }),
-    null_ls.builtins.code_actions.eslint_d.with({
+    none_ls.builtins.code_actions.eslint_d.with({
       condition = function(utils)
         return utils.root_has_file({ "package.json" })
             and utils.root_has_file_matches(".eslintrc.*")
       end,
     }),
-    null_ls.builtins.code_actions.gitsigns,
+    none_ls.builtins.code_actions.gitsigns,
   },
 })
