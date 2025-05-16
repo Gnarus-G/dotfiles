6ce1f62729db1abf61be21a08d2ab4c1b99a6a79
gnarus-g <g.avet.gnaritas@gmail.com>
2025-05-16 08:46:52 -0400

    Fix todolang settings

--- .config/nvim/after/plugin/todolang.lua
+++ .config/nvim/after/plugin/todolang.lua
@@ -22,3 +22,24 @@ vim.api.nvim_create_autocmd('LspAttach', {
     })
   end
 })
+
+
+-- Syntax higlghlighting for todolang using tree-sitter
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.todolang = {
+  install_info = {
+    url = "https://github.com/Gnarus-G/tree-sitter-todolang", -- local path or git repo
+    files = { "src/parser.c" },                               -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main",                                          -- default branch in case of git repo if different from master
+    generate_requires_npm = false,                            -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false,                   -- if folder contains pre-generated src/parser.c
+  },
+}
+
+vim.filetype.add({
+  extension = {
+    todo = "todolang",
+    td = "todolang"
+  }
+})
