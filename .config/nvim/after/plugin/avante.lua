0711b9be2258a1ae8cb351cd6d5e223e8351b35b
gnarus-g <g.avet.gnaritas@gmail.com>
2025-05-21 12:26:22 -0400

    Re-enable rag with avante

--- .config/nvim/after/plugin/avante.lua
+++ .config/nvim/after/plugin/avante.lua
@@ -56,7 +56,7 @@ local config = {
     enable_cursor_planning_mode = true, -- enable cursor planning mode!
   },
   rag_service = {
-    enabled = false,                        -- Enables the RAG service
+    enabled = true,                         -- Enables the RAG service
     host_mount = os.getenv("HOME") .. "/d", -- Host mount path for the rag service
     provider = "ollama",                    -- The provider to use for RAG service (e.g. openai or ollama)
     llm_model = "gemma3",                   -- The LLM model to use for RAG service
