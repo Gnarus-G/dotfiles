a3d176e2c40461e0212eed6e9f2c9e29a02a0b20
gnarus-g <g.avet.gnaritas@gmail.com>
2025-05-25 03:24:40 -0400

    Update some minor things

--- .config/nvim/after/plugin/mcphub.lua
+++ .config/nvim/after/plugin/mcphub.lua
@@ -1,3 +1,6 @@
+-- some extra mcp servers to reinstall:
+-- web_search: https://github.com/ihor-sokoliuk/mcp-searxng, https://github.com/Gnarus-G/ez-web-search-mcp
+
 require("mcphub").setup({
   port = 37373,                                            -- Default port for MCP Hub
   config = vim.fn.expand("~/.config/mcphub/servers.json"), -- Absolute path to config file location (will create if not exists)
