2db32d4b3b9ddfc467ab6b3820c57b771d6934a1
gnarus-g <g.avet.gnaritas@gmail.com>
2025-08-04 16:18:59 -0400

    Fix refactoring plugin configuration

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -4,7 +4,43 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  lazy = false,
+  opts = {},
+  keys = {
+    -- prompt for a refactor to apply when the remap is triggered
+    {
+      "<leader>rr",
+      function() require('refactoring').select_refactor() end,
+      mode = { "n", "x" },
+    }
+    -- Note that not all refactor support both normal and visual mode
+  },
   config = function()
-    require('refactoring').setup()
-  end,
+    require('refactoring').setup({
+      prompt_func_return_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+      },
+      prompt_func_param_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+      },
+      printf_statements = {},
+      print_var_statements = {},
+      show_success_message = false, -- shows a message with information about the refactor on success
+      -- i.e. [Refactor] Inlined 3 variable occurrences
+    })
+  end
 }
