fc1d9824eaa46a3c3333fe08db262e8a6dde84ea
gnarus-g <g.avet.gnaritas@gmail.com>
2025-08-04 15:44:09 -0400

    Add hidden file live grep and improve keymap formatting
    Improve keymaps by adding live grep with hidden files support.
    Also, format multi-line keymap entries for better readability.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,22 +3,50 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = "Telescope",
   keys = {
-    { "<C-p>", function()
-      require("telescope.builtin").find_files({ find_command = { "rg", "--ignore", "--files" }, prompt_prefix = "🔍" })
-    end, desc = "Find files" },
-    { "<leader>ff", function()
-      require("telescope.builtin").find_files({ find_command = { "rg", "--ignore", "--hidden", "--files" }, prompt_prefix = "🔍" })
-    end, desc = "Find files (hidden)" },
-    { "<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
-    { "<leader>fo", function() require("telescope.builtin").oldfiles() end, desc = "Old files" },
-    { "<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Buffers" },
-    { "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
+    {
+      "<C-p>",
+      function()
+        require("telescope.builtin").find_files({ find_command = { "rg", "--ignore", "--files" }, prompt_prefix = "🔍" })
+      end,
+      desc = "Find files"
+    },
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({
+          find_command = { "rg", "--ignore", "--hidden", "--files" },
+          prompt_prefix =
+          "🔍"
+        })
+      end,
+      desc = "Find files (hidden)"
+    },
+    { "<leader>fg", function() require("telescope.builtin").live_grep() end,      desc = "Live grep" },
+    {
+      "<leader>hg",
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = function(_)
+            return { "--hidden", "--glob", "!**/.git/*" }
+          end,
+        })
+      end,
+      desc = "Live grep (hidden files)"
+    },
+    { "<leader>fo", function() require("telescope.builtin").oldfiles() end,       desc = "Old files" },
+    { "<leader>fb", function() require("telescope.builtin").buffers() end,        desc = "Buffers" },
+    { "<leader>fh", function() require("telescope.builtin").help_tags() end,      desc = "Help tags" },
     { "<leader>fr", function() require("telescope.builtin").lsp_references() end, desc = "LSP references" },
-    { "<leader>gc", function() require("telescope.builtin").git_bcommits() end, desc = "Git buffer commits" },
-    { "<leader>gC", function() require("telescope.builtin").git_commits() end, desc = "Git commits" },
-    { "<leader>rr", function()
-      require('telescope').extensions.refactoring.refactors()
-    end, mode = { "n", "x" }, desc = "Refactoring" },
+    { "<leader>gc", function() require("telescope.builtin").git_bcommits() end,   desc = "Git buffer commits" },
+    { "<leader>gC", function() require("telescope.builtin").git_commits() end,    desc = "Git commits" },
+    {
+      "<leader>rr",
+      function()
+        require('telescope').extensions.refactoring.refactors()
+      end,
+      mode = { "n", "x" },
+      desc = "Refactoring"
+    },
   },
   config = function()
     require("telescope").setup({
@@ -32,7 +60,6 @@ return {
       pickers = {},
       extensions = {}
     })
-    
     require("telescope").load_extension("refactoring")
   end,
 }
