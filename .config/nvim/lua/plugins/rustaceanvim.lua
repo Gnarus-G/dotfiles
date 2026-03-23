28e5f33b1819866c68cdb0b9642d050b07155546
gnarus-g <g.avet.gnaritas@gmail.com>
2026-03-23 09:43:48 -0400

    Remove manual codelldb DAP adapter setup
    Replace explicit DAP adapter configuration with rustaceanvim's built-in DAP support, simplifying setup and reducing maintenance overhead.

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -3,18 +3,8 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^8',
     lazy = false,
-    init = function()
-      local mason_registry = require('mason-registry')
-      local codelldb = mason_registry.get_package('codelldb')
-      local extension_path = codelldb:get_install_path() .. '/extension/'
-      local codelldb_path = extension_path .. 'adapter/codelldb'
-      local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
-
-      ---@type rustaceanvim.Opts
+    config = function()
       vim.g.rustaceanvim = {
-        dap = {
-          adapter = require('rustaceanvim.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
         server = {
           default_settings = {
             ['rust-analyzer'] = {
