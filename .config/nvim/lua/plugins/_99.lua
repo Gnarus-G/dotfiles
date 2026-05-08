09b4e63ae3963792ce5fda5d9f6fe56b3a208f8a
gnarus-g <g.avet.gnaritas@gmail.com>
2026-05-08 17:07:24 -0400

    Enable 99 display errors and add debug/model/provider keymaps
    Turn on error display for the 99 plugin and add new normal mode
    keybindings for viewing logs, clearing request history, showing
    current status, selecting model and provider via telescope, and
    controlling worker actions (set work and search).

--- .config/nvim/lua/plugins/_99.lua
+++ .config/nvim/lua/plugins/_99.lua
@@ -6,7 +6,6 @@ return {
     -- For logging that is to a file if you wish to trace through requests
     -- for reporting bugs, i would not rely on this, but instead the provided
     -- logging mechanisms within 99.  This is for more debugging purposes
-    ---@diagnostic disable: undefined-field
     local cwd = vim.uv.cwd()
     local basename = vim.fs.basename(cwd)
 
@@ -46,6 +45,7 @@ return {
       -- https://code.claude.com/docs/en/permissions#read-and-edit
       tmp_dir = "/tmp", -- I allowed /tmp/** in opencode
       show_in_flight_requests = true,
+      display_errors = true,
       auto_add_skills = true,
 
       --- Completions: #rules and @files in the prompt buffer
@@ -142,6 +142,41 @@ return {
     vim.keymap.set("n", "<leader>9o", function()
       _99.open()
     end)
+
+    --- view last run's debug logs
+    vim.keymap.set("n", "<leader>9l", function()
+      _99.view_logs()
+    end, { desc = "99 view logs" })
+
+    --- clear all previous request history
+    vim.keymap.set("n", "<leader>9c", function()
+      _99.clear_previous_requests()
+    end, { desc = "99 clear requests" })
+
+    --- show current state (request count, custom rules)
+    vim.keymap.set("n", "<leader>9i", function()
+      _99.info()
+    end, { desc = "99 info" })
+
+    --- select model via telescope
+    vim.keymap.set("n", "<leader>9m", function()
+      require("99.extensions.telescope").select_model()
+    end, { desc = "99 select model" })
+
+    --- select provider via telescope
+    vim.keymap.set("n", "<leader>9p", function()
+      require("99.extensions.telescope").select_provider()
+    end, { desc = "99 select provider" })
+
+    --- Worker: set work description
+    vim.keymap.set("n", "<leader>9w", function()
+      _99.Extensions.Worker.set_work()
+    end, { desc = "99 set work" })
+
+    --- Worker: search remaining work
+    vim.keymap.set("n", "<leader>9W", function()
+      _99.Extensions.Worker.search()
+    end, { desc = "99 work search" })
 
     --- add md_files via file picker (markdown only)
     vim.keymap.set("n", "<leader>9a", function()
